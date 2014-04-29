23e50aa5e31059da7a37df0ae84f255ae5d06a5c
Josh Zehner <JZehner@exacttarget.com>
2014-04-29 18:15:23 -0400

    added welcome trigger word for incoming

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var Slack = require('node-slack');
 var slackDomain = 'et-jbdbc'; 
 //var slackToken = 'eqAJpc6Z6iyOI2gnMRFhkG65';
-var slackToken = 'MwfFA0lvXsvB6fQHkqIYklJr';
+//Mobile
+//var slackToken = 'MwfFA0lvXsvB6fQHkqIYklJr';
+var slackToken = 'peaMT9FiqLHrYxyybld0jmYc';
 var slack = new Slack(slackDomain,slackToken);
 var router = express.Router();
 
@@ -12,15 +14,34 @@ router.get('/', function(req, res) {
 });
 
 router.post('/incoming', function(req, res){
-    var reply = slack.respond(req.body,function(hook){
-        return {
-            text: 'Test reply to ' + hook.user_name,
-            username: 'Hotel'
-        };
-    });
+    var reply = {};
+    switch(req.body.trigger_word){
+        case 'hi':
+            reply = slack.respond(req.body,function(hook){
+                return {
+                    text: 'Test reply to ' + hook.user_name,
+                    username: 'Hotel'
+                };
+            });
+            break;
+        case 'welcome':
+            reply = slack.respond(req.body, function(hook){
+                return {
+                    text: 'Welcome to you',
+                    username:'Hotel'
+                }
+            });
+            break;
+        default:
+            break;
+    }
+    
+    
+    
     
     res.json(reply);
 });
+
 
 router.post('/sendmessage', function(req, res){
     var post = slack.send({
